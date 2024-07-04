16e690195c6b108ea006cbda0b0b23a796e8d286
Raziel Melchor <razielmelchor@gmail.com>
2024-07-03 22:03:16 -0700

    initial commit

---
+++ HandMadeHero/HandMadeHero.cpp
@@ -0,0 +1,30 @@
+// HandMadeHero.cpp : Defines the entry point for the application.
+//
+
+#include "HandMadeHero.h"
+#include "windows.h"    
+
+void foo() {
+    OutputDebugStringA("Hello World\n");
+
+}
+
+int CALLBACK  WinMain( HINSTANCE hInstance,
+                      HINSTANCE hPrevInstance,
+                      LPSTR    lpCmdLine,
+                      int       nCmdShow)
+{
+
+    // TODO: Place code here.
+
+    foo();  
+
+    int x = 0;
+    x += 1;
+
+    
+
+}
+
+
+
---
+++ test.cpp
@@ -0,0 +1,8 @@
+
+#include <Winbase.h>
+#include <windows.h>
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+	return 0;
+};
