443047e33df44473349b3124750454c1798d8213
Raziel Melchor <razielmelchor@gmail.com>
2024-12-31 12:22:12 -0800

    add to gitignore

--- HandMadeHero/HandMadeHero.cpp
+++ HandMadeHero/HandMadeHero.cpp
@@ -508,6 +508,7 @@ int CALLBACK WinMain(HINSTANCE Instance,
 						{
 
 							real32 SineValue;
+
 							int16 SampleValue = (RunningSampleIndex / (HalfWavePeriod) ) %2 ? ToneVolume : -ToneVolume;
 
 							*SampleOut++ = SampleValue;
